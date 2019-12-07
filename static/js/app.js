b5d8a9052378c6e8367d714c9a3b40e1d7fa1958
brendanoshea84 <b_oshea_84@hotmail.com>
2019-12-07 12:47:50 +0100

    connect input on form to change name for players on HTML

--- static/js/app.js
+++ static/js/app.js
@@ -31,8 +31,87 @@ $('.dice').click(function() {
 });
 
 
+//Mondal Options
 
-/*Mondal open on page ready*/
+//Mondal open on page ready
 $(document).ready(function() {
     $("#introMondal").modal('show')
+});
+
+//close mondal
+function closeMondal() {
+    $("#introMondal").modal('hide');
+};
+
+
+
+
+//Get Local players Name from Mondal 
+
+let player1;
+let player2;
+let player1Name;
+let player2Name;
+
+function getPlayerData() {
+    //Get local player names from form input
+    var player1 = document.getElementById("userId").elements.namedItem("player1").value;
+    var player2 = document.getElementById("userId").elements.namedItem("player2").value;
+
+    //Change the name from HTML
+    let player1Name = document.getElementsByClassName("player-0-name");
+    let player2Name = document.getElementsByClassName("player-1-name");
+
+    for (var i = 0; i < player1Name.length; i++) {
+        player1Name[i].innerHTML = player1;
+        player2Name[i].innerHTML = player2;
+
+    }
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Start btn
+- save all the players data to storage
+- check to see if its player V player or against computer
+- clear history from previse game
+- set score to 0 - 0
+- random player goes first
+- close mondal to see game
+
+*/
+document.getElementById("start-btn").addEventListener("click", function() {
+
+    //Data saved
+    getPlayerData();
+
+
+
+
+
+
+    //computer as player check
+
+
+    //reset score and history
+
+    //random player start
+
+    //close modal
+    closeMondal();
 });
